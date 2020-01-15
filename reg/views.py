46ec2a6ed9c996a955b28e1e2f337737196e2a34
Lanting Chiang <49918461+lantingchiang@users.noreply.github.com>
2020-01-15 17:42:56 -0500

    second commit

--- reg/views.py
+++ reg/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event, User, EventForm, SignupForm
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+# home page for user log in
 def index(request):
+    
+
     return HttpResponse("This should be the home login page")
+
+# user sign up page
+def signup(request):
+    form = SignupForm()    
+    return render(request, 'signup.html', {'form': form})
+
+# event viewing page
+def dashboard(request):
+    event_list = Event.objects.order_by('-event_time')
+    context = {'event_list': event_list}
+    return render(request, 'reg/dashboard.html', context)
+
+# event creation page
+def create_event(request):
+    form = EventForm()    
+    return render(request, 'create-event.html', {'form': form})
+
+
